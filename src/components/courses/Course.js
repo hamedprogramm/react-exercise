063e4f477d5b009ad0cd081a7c63f3db88a93598
hamed <81282529+hamedprogramm@users.noreply.github.com>
2022-07-13 19:27:18 +0430

    add (up or minus the quantity)
    in this feature you can choose quantity of each courses

--- src/components/courses/Course.js
+++ src/components/courses/Course.js
@@ -1,18 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Course = (props) => {
+const Course = ({ title, hours, teacher, price }) => {
+  const [change, setChange] = useState({
+    showMe: false,
+  });
+
+  const [count, setCount] = useState(1);
+
+  const changeEl = () => {
+    setChange({
+      showMe: true,
+    });
+  };
+
+  const addOneHandler = () => {
+    setCount((prevState) => {
+      return prevState + 1;
+    });
+  };
+
+  const minusOneHandler = () => {
+    setCount((prevState) => {
+      return prevState - 1;
+    });
+  };
+
   return (
     <div className="course">
       <div className="course-name">
-        <h1 className="c-name">{props.title}</h1>
+        <h1 className="c-name">{title}</h1>
       </div>
       <div className="course-info">
-        <p className="c-hour">Hour : {props.hours} hours</p>
-        <p className="c-teach">Teacher : {props.teacher} {(props.teacher === 'saheb') ? '(confirmed)' : ''} </p>
-        <p className="c-price">price : {props.price} $</p>
+        <p className="c-hour">Hour : {hours} hours</p>
+        <p className="c-teach">
+          Teacher : {teacher} {teacher === "saheb" ? "(confirmed)" : ""}
+        </p>
+        <p className="c-price">price : {price} $</p>
       </div>
       <div className="buy-btn">
-        <button className="buy">buy now !</button>
+        {(change.showMe === false || count == 0) ? (
+          <button onClick={changeEl} className="buy">
+            buy now !
+          </button>
+        ) : (
+          <>
+            <button onClick={addOneHandler} className="up">
+              +
+            </button>
+            <p className="quantityShow">{count}</p>
+            <button onClick={minusOneHandler} className="minus">
+              -
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
